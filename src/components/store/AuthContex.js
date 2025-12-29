fb918c641e2c43d5e02b8057f8bd4483bab2f902
Rajeev Kumar <rajeevkumarrj7@gmail.com>
2025-12-29 13:18:27 +0530

    added auto logout

--- src/components/store/AuthContex.js
+++ src/components/store/AuthContex.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   token: "",
@@ -8,8 +8,8 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-
-  const storedToken=localStorage.getItem("token");
+  const storedToken = localStorage.getItem("token");
+  const storedTime = localStorage.getItem("loginTime"); // timestamp in ms
   const [token, setToken] = useState(storedToken);
 
   const isLoggedIn = !!token;
@@ -17,20 +17,32 @@ export const AuthContextProvider = (props) => {
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
+    localStorage.removeItem("loginTime");
   };
 
   const loginHandler = (token) => {
-    
     setToken(token);
-    localStorage.setItem("token",token);
+    localStorage.setItem("token", token);
+    localStorage.setItem("loginTime", Date.now());
   };
 
+  useEffect(() => {
+    if (storedToken && storedTime) {
+      const remainingTime = 5 * 60 * 1000 - (Date.now() - parseInt(storedTime));
+      if (remainingTime <= 0) {
+        logoutHandler();
+      } else {
+        const timer = setTimeout(logoutHandler, remainingTime);
+        return () => clearTimeout(timer);
+      }
+    }
+  }, [storedToken, storedTime]);
 
   return (
     <AuthContext.Provider
       value={{
-        token:token,
-        isLoggedIn:isLoggedIn,
+        token: token,
+        isLoggedIn: isLoggedIn,
         login: loginHandler,
         logout: logoutHandler,
       }}
